98cd7fa6a62cfa3ce0228fc02d45df147c0e191d
Ajay <ajaynimmala18@gmail.com>
2024-07-26 16:06:36 +0530

    P1 solved

--- Arrays/P1.py
+++ Arrays/P1.py
@@ -5,3 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        self.nums = nums
+        self.target = target
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return (i, j)
