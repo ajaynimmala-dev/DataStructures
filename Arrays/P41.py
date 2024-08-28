c3e2153662c71a546fa31d979c7199d3a789dc28
Ajay <ajaynimmala18@gmail.com>
2024-08-28 22:22:18 +0530

    Problem solved

--- Arrays/P41.py
+++ Arrays/P41.py
@@ -4,12 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        i=1
-        set(nums)
+        i = 1
+        nums = set(nums)
+        nums = list(nums)
+        nums = [x for x in nums if x > 0]
         nums.sort()
+        if 1 not in nums:
+            return (1)
+        else:
+            for i in range(1, len(nums)):
+                if nums[i] != nums[i - 1] + 1:
+                    return (nums[i - 1] + 1)
+            return (max(nums) + 1)
 
-        while i<=len(nums):
-            if i not in nums:
-                return(i)
-            i+=1
-        return(max(nums)+1)
+
+
+
+
