c95e35038bc96ac16004abdd4ace99cf69ab3d5b
Ajay <ajaynimmala18@gmail.com>
2024-09-02 10:32:34 +0530

    Problem updated

--- Arrays/P53.py
+++ Arrays/P53.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        res = []
+        res = nums[0]
         for i in range(len(nums)):
-            res.append(nums[i])
+            res = max(nums[i], res)
             for j in range(i + 1, len(nums)):
                 x = sum([nums[k] for k in range(i, j + 1)])
-                res.append(x)
-        return (max(res))
+                res = max(res, x)
+        return (res)
