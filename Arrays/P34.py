3f8865ea2029f8a0ead170d2c3d8a3c762d33aac
Ajay <ajaynimmala18@gmail.com>
2025-01-06 19:02:06 +0530

    problems

---
+++ Arrays/P34.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if target in nums:
+            count_target = nums.count(target) - 1
+            find_target = nums.index(target)
+            return ([find_target, find_target + count_target])
+        else:
+            return ([-1, -1])
+
